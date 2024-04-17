29e0ac29884bc60a1518b01d1f9ce332d3885921
MVC-56 <MVC-56@DESKTOP-50D53D5>
2024-04-18 02:13:16 +0300

    Добавьте файлы проекта.

---
+++ ProjectManager/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Calendar.Main();
+            Process.Start("ProjectOne.exe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProjectTwo.Form1 form1 = new ProjectTwo.Form1();
+            form1.Show();
+        }
+    }
+}
---
+++ ProjectOne/Calendar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ProjectOne
+{
+    internal class Calendar
+    {
+        public static void Main()
+        {
+            DateTime now = GetCurrentDate();
+            Console.WriteLine($"Today's date is {now}");
+            Console.ReadLine();
+        }
+
+        static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+    }
+}
